527b72a8b1a8171bad33a7e305b99dff63e42a8b
Joe Rehfuss <joe.rehfuss@gmail.com>
2022-08-01 15:24:52 -0700

    adding routing

--- models/index.js
+++ models/index.js
@@ -2,10 +2,14 @@ const Trip = require("./Trip");
 const Traveller = require("./Traveller");
 const Location = require("./Location");
 
-Location.hasMany(Trip);
+Location.hasMany(Trip,{
+    onDelete:"CASCADE"
+});
 Trip.belongsTo(Location);
 
-Traveller.hasMany(Trip);
+Traveller.hasMany(Trip,{
+    onDelete:"CASCADE"
+});
 Trip.belongsTo(Traveller);
 
 module.exports={
